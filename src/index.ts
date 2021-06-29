bf8b31e77b203ca48fb9d7a898c79b9dc11718df
pewww <jung22337@naver.com>
2021-06-29 14:26:14 +0900

    :sparkles: Add tutorial

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,51 @@ import * as queryString from 'query-string';
 
 import Main from './models/Main';
 import Game from './models/Game';
+import Popup from './models/Popup';
 import { OBJECT_STATUS_BY_LEVEL, TITLE_FONT_SIZE_BY_LEVEL } from './constants/level';
-import { $id } from './utils/dom';
+import { $id, $class } from './utils/dom';
 
 const parsedQuery = queryString.parse(location.search);
-const level = Number((parsedQuery as Record<string, unknown>).level ?? '1');
+const level = Number((parsedQuery as Record<string, unknown>).level);
 
-new Main(
-  new Game(
-    OBJECT_STATUS_BY_LEVEL[level].stageSize,
-    OBJECT_STATUS_BY_LEVEL[level].blockSize,
-    level
-  )
-).render();
+if (level) {
+  new Main(
+    new Game(
+      OBJECT_STATUS_BY_LEVEL[level].stageSize,
+      OBJECT_STATUS_BY_LEVEL[level].blockSize,
+      level
+    )
+  ).render();
+  
+  const levelTitleElement = $id('level-title');
+  const rootElement = $id('root');
+  
+  levelTitleElement.innerText = `LEVEL ${level}`;
+  levelTitleElement.style.fontSize = `${TITLE_FONT_SIZE_BY_LEVEL[level]}px`;
+  levelTitleElement.style.marginTop = `-${rootElement.clientHeight + 100}px`;
+} else {
+  const gameConnectionCoverElement = $id('game-connection-cover');
 
-const titleElement = $id('title');
-const rootElement = $id('root');
+  gameConnectionCoverElement.style.display = 'block';
 
-titleElement.innerText = `LEVEL ${level}`;
-titleElement.style.fontSize = `${TITLE_FONT_SIZE_BY_LEVEL[level]}px`;
-titleElement.style.marginTop = `-${rootElement.clientHeight + 100}px`;
+  const gameStartBtnElement = $id('game-start-btn');
+  const howToBtnElement = $id('how-to-btn');
+  const popup = new Popup('how-to-popup-wrapper');
+
+  const handleGameStartBtnClick = () => {
+    location.href = `${location.pathname}?level=1`;
+  };
+
+  const handleHowToBtnClick = () => {
+    const closeBtn = $class('close-btn');
+
+    popup.open();
+
+    closeBtn[0].addEventListener('click', () => {
+      popup.close();
+    });
+  };
+
+  gameStartBtnElement.addEventListener('click', handleGameStartBtnClick);
+  howToBtnElement.addEventListener('click', handleHowToBtnClick);
+}
--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,2 +1,3 @@
 export const $create = (tag: string) => document.createElement(tag);
 export const $id = document.getElementById.bind(document);
+export const $class = document.getElementsByClassName.bind(document);
